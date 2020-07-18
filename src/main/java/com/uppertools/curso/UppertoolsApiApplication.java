5f15f6ae1fbc901ab2f2f52fae92b2cf09a530b2
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-18 17:48:03 -0300

    Arquivos iniciais

---
+++ src/main/java/com/uppertools/curso/UppertoolsApiApplication.java
@@ -0,0 +1,13 @@
+package com.uppertools.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppertoolsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppertoolsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uppertools/curso/UppertoolsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uppertools.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppertoolsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
