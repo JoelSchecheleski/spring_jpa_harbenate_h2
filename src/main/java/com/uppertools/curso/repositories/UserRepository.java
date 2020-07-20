3e73f1fb209c7b7c75f7ccbe49c1d9c8af02e27e
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-20 13:53:36 -0300

    Service layer, component registration

--- src/main/java/com/uppertools/curso/repositories/UserRepository.java
+++ src/main/java/com/uppertools/curso/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.uppertools.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.uppertools.curso.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	
-	
-	
+
 }
