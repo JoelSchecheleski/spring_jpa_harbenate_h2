93db10b2b44d24b3212e88e3703be307dd00127c
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-21 18:51:17 -0300

    Category

--- src/main/java/com/uppertools/curso/config/TestConfig.java
+++ src/main/java/com/uppertools/curso/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.uppertools.curso.entities.Category;
 import com.uppertools.curso.entities.Order;
 import com.uppertools.curso.entities.User;
 import com.uppertools.curso.entities.enums.OrderStatus;
+import com.uppertools.curso.repositories.CategoryRepository;
 import com.uppertools.curso.repositories.OrderRepository;
 import com.uppertools.curso.repositories.UserRepository;
 
@@ -23,9 +25,17 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository; 
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Joel Schecheleski", "joel.jsp@gmail.com", "47996688829", "123456789");
 		User u2 = new User(null, "Kátia Michele Jurk Avalos Schecheleski", "kvalos@gmail.com", "", "123456789");
 		User u3 = new User(null, "Miguel Schecheleski", "miguel@gmail.com", "", "123456789");
@@ -39,7 +49,7 @@ public class TestConfig implements CommandLineRunner {
 		Order o5 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		Order o6 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u2);
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6));
-
+		
 	}
 
 }
