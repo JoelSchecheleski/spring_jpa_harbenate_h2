3a5d332eb81a897c9e5dd0904fc2a9fa92ad75c9
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-22 14:44:32 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/uppertools/curso/entities/Product.java
+++ src/main/java/com/uppertools/curso/entities/Product.java
@@ -11,8 +11,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -33,6 +36,9 @@ public class Product implements Serializable {
 	@JoinTable(name = "tb_prodcut_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>(); // Garante que não terá categoria repetida
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 	}
 
@@ -87,6 +93,16 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	// Busca todas as orders no qual o produto se envolvel
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
