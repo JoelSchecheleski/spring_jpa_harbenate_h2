9a5b49e8af6e382f48ac4030deb003017dae8e8b
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-22 20:58:51 -0300

    Exception handling - findById

---
+++ src/main/java/com/uppertools/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.uppertools.curso.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.uppertools.curso.services.exceptions.ResourceNotFoundException;
+
+/**
+ * Interceta as exeções para ser executado os possíveis tratamento
+ * 
+ * @author Joel_
+ *
+ */
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
