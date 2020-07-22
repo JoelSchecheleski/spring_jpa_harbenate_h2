8ac9d360acb28bac08ea184b9e1a4735dfac8740
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-22 17:41:05 -0300

    User insert

--- src/main/java/com/uppertools/curso/resources/UserResource.java
+++ src/main/java/com/uppertools/curso/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.uppertools.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.uppertools.curso.entities.User;
 import com.uppertools.curso.services.UserService;
@@ -19,16 +23,40 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
+	/**
+	 * Busca todos os usuários
+	 * 
+	 * @return Retorna um array de objetos do tipo usuário
+	 */
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
+	/**
+	 * Busca um usuário pelo id
+	 * 
+	 * @param id Código do usuário da requisição path
+	 * @return Retorna o usuário localizado pelo id
+	 */
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok(obj);
+	}
+
+	/**
+	 * Cria um novo usuário
+	 * 
+	 * @param obj Objeto da requisição
+	 * @return Objeto user inserido
+	 */
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
--- src/main/java/com/uppertools/curso/services/UserService.java
+++ src/main/java/com/uppertools/curso/services/UserService.java
@@ -15,12 +15,37 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	/**
+	 * Busca todos os usuários cadatrados no sistema
+	 * @return Retorna um array de objetos Users
+	 */
 	public List<User> findAll(){
 		return repository.findAll();
 	}
 	
+	/**
+	 * Busca um usuário pelo ID
+	 * @param id Código do usuário a ser localizado
+	 * @return Retorna um usuário pelo id
+	 */
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	/**
+	 * Salva no banco de dados um novo objeto do tipo User.
+	 * @param obj Objeto recebido no contexto.
+	 * @return Retorna o objeto inserido.
+	 */
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	
+	public User delete(User obj) {
+		
+	}
+	
+	
 }
